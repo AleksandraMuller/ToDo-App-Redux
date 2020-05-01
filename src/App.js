1c41e4c4591583b0f9ea2cee75bbd5b2c1b78ac0
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-05-01 22:39:20 +0200

    create react app start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { TaskList } from "./components/TaskList";
+
+export const App = () => {
+  return (
+    <div>
+      <TaskList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TaskList = () => {
+  return (
+    <div>
+      <h1>My ToDO - App</h1>
+    </div>
+  );
+};
